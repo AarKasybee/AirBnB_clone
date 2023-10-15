bfa53b854981d3ec07cffb5721b5eb30fc083ebb
Beethoven Kasompe <kasbeet@gmail.com>
2023-10-15 03:49:53 -0700

    added basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""Define class : BaseModel"""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """BaseModel"""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
