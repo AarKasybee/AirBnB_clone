ba6d301cbe74fd2c0f88a09ffae5eab04cb2e4bc
Beethoven Kasompe <kasbeet@gmail.com>
2023-10-15 02:13:02 -0700

    basemodel tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""BaseModel that defines all common attributes/methods for other classes"""
+"""Defines the BaseModel class."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
@@ -15,8 +23,8 @@ class BaseModel:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
                     self.__dict__[k] = datetime.strptime(v, tform)
-                    else:
-                        self.__dict__[k] = v
+                else:
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
